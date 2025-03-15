336d0dca51ec6b6b377d939739a5b7281497ab47
Nikita Goncharov <nikitagoncarov657@gmail.com>
2025-03-15 10:19:20 +0200

    oooooops & restore deleted

--- bot.py
+++ bot.py
@@ -193,9 +193,141 @@ async def process_remove_keyword(message: Message, state: FSMContext):
         )
     
     await state.clear()
+    
+
+@dp.message(F.text.lower() == "additional text")
+async def show_additional_text(message: Message):
+    """
+    Show current additional text and inline keyboard for adding/removing.
+    """
+    if not global_additional_text:
+        await message.answer(
+            "No additional text added.",
+            reply_markup=inline_kb_additional_text
+        )
+    else:
+        await message.answer(
+            f"Current additional text:\n<b>{global_additional_text}</b>",
+            reply_markup=inline_kb_additional_text,
+            parse_mode="html"
+        )
 
 
-# ---- Entry point for the bot ----
+@dp.callback_query(F.data == "add_text")
+async def callback_add_text(callback: CallbackQuery, state: FSMContext):
+    """
+    We ask the user for a line to add to the additional texts.
+    """
+    await callback.message.answer("Enter an additional text to add:")
+    await state.set_state(AddAdditionalTextState.waiting_for_text)
+
+
+@dp.callback_query(F.data == "remove_text")
+async def callback_remove_text(callback: CallbackQuery):
+    """
+    Delete additional text.
+    """
+    global global_additional_text
+    global_additional_text = ""
+    
+    await callback.message.answer(
+        "Removed additional text.",
+        parse_mode="html",
+        reply_markup=kb_main
+    )
+
+
+@dp.message(AddAdditionalTextState.waiting_for_text)
+async def process_add_text(message: Message, state: FSMContext):
+    """
+    Processing the entered string to add to additional texts.
+    """
+    global global_additional_text
+
+    text_item = message.text.strip().lower()
+    if text_item == global_additional_text:
+        await message.answer(
+            f"Text <b>{text_item}</b> already in the list.",
+            parse_mode="html",
+            reply_markup=kb_main
+        )
+    else:
+        global_additional_text = text_item
+        await message.answer(
+            f"Added <b>{text_item}</b> as the additional text.",
+            parse_mode="html",
+            reply_markup=kb_main
+        )
+
+    await state.clear()
+
+
+@dp.message(F.text.lower() == "start search")
+async def start_search_button(message: Message):
+    """
+    Launch the parser with an existing list of keywords and additional text.
+    """
+    global crawler
+
+    if not global_keywords:
+        await message.answer(
+            "There are no keywords. You should add some first.",
+            reply_markup=kb_main,
+            parse_mode="html",
+        )
+        return
+
+    if crawler is not None:
+        crawler.stop_parsing()
+        crawler = None
+    
+    keywords_list = list(global_keywords)
+
+    
+    from crawler import Crawler
+    crawler = Crawler(keywords_list, global_additional_text)
+    crawler.start_parsing()
+
+    await message.answer(
+        "Crawling started.\n"
+        f"Keywords: {', '.join(keywords_list)}\n"
+        f"Additional text: {global_additional_text if global_additional_text else 'No additional text'}\n"
+        "To stop crawling press: Stop Search",
+        reply_markup=kb_main,
+        parse_mode="html",
+    )
+
+
+@dp.message(F.text.lower() == "stop search")
+async def stop_search_button(message: Message):
+    """
+    Stop crawling and send link to archive
+    """
+    global crawler
+    if crawler is not None:
+        crawler.stop_parsing()
+        crawler = None
+
+    await message.answer("Crawling stopped.", reply_markup=kb_main, parse_mode="html")
+    crawled_links_count = redis_client.get("crawled_links_count").decode()
+    saved_images_count = redis_client.get("saved_images_count").decode()
+    await message.answer(f"Statistics: \nCrawled links count: {crawled_links_count}\nSaved images count: {saved_images_count}")
+
+    # Link to images archive
+    archive_url = f'<a href="http://{config.SERVER_HOST_HUMANABLE}:{config.SERVER_PORT}/get_images_archive">Download</a>'
+    await message.answer(
+        f"📁 Archive with parsed images you can get here: {archive_url}",
+        reply_markup=kb_main,
+        parse_mode="HTML",
+    )
+    
+    # remove unique images hashes
+    image_hashes = redis_client.smembers("image_hashes")
+    redis_client.srem("image_hashes", *image_hashes)
+    redis_client.set("crawled_links_count", 0)
+    redis_client.set("saved_images_count", 0)
+
+
 async def main():
     await dp.start_polling(bot)
 
